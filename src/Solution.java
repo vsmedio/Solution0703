ef3949da9739b7785d48b2a2cb142536ded1ee15
Arvis Abromovics <info@vsmedio.lv>
2018-01-07 20:09:08 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Общение одиноких массивов
+1. Создать массив на 10 строк.
+2. Создать массив на 10 чисел.
+3. Ввести с клавиатуры 10 строк, заполнить ими массив строк.
+4. В каждую ячейку массива чисел записать длину строки из массива строк, индекс/номер ячейки которой совпадает с
+текущим индексом из массива чисел.
+
+Вывести содержимое массива чисел на экран, каждое значение выводить с новой строки.
+
+
+Требования:
+1. Программа должна создавать массив на 10 строк.
+2. Программа должна создавать массив на 10 целых чисел.
+3. Программа должна считывать строки для массива с клавиатуры.
+4. Программа должна в массив чисел записать длины строк из массива строк, а затем их вывести на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        String[] strings = new String[10];
+        int[] ints = new int[10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = reader.readLine();
+            ints[i] = strings[i].length();
+        }
+
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+}
